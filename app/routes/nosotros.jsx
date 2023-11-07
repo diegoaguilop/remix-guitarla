e273bc2ab10d1e55d3a155fa7ca3e52b5fbf5849
diegoaguilop <diego.aguilar1811@gmail.com>
2023-11-07 14:10:09 +0100

    Continuacion proyecto

--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,6 +1,41 @@
+import imagen from "../../public/img/nosotros.jpg"
+import styles from "../styles/nosotros.css"
+
+export function meta() {
+    return {
+        title: "GuitarLA - Sobre Nosotros",
+        description: "Venta de guitarras, blog de música"
+    }
+}
+
+export function links() {
+    return [
+        {
+            rel: "stylesheet",
+            href: styles
+        },
+        {
+            rel: "preload",
+            href: imagen,
+            as: "image"
+        }
+    ]
+}
+
 function Nosotros() {
     return (
-        <div>nosotros</div>
+        <main className="contenedor nosotros">
+            <h2 className="heading">Nosotros</h2>
+
+            <div className="contenido">
+                <img src={imagen} alt="Imagen sobre nosotros" />
+                <div>
+                    <p>Et fugiat esse dolor fugiat excepteur amet duis reprehenderit aute cupidatat incididunt laboris voluptate. Velit velit adipisicing adipisicing reprehenderit. Cillum est ut et occaecat esse consequat ut. Nisi aliqua consequat tempor laborum id eu ex est eu commodo do ea. Exercitation duis velit velit nulla commodo consequat eu labore minim.</p>
+
+                    <p>Et fugiat esse dolor fugiat excepteur amet duis reprehenderit aute cupidatat incididunt laboris voluptate. Velit velit adipisicing adipisicing reprehenderit. Cillum est ut et occaecat esse consequat ut. Nisi aliqua consequat tempor laborum id eu ex est eu commodo do ea. Exercitation duis velit velit nulla commodo consequat eu labore minim.</p>
+                </div>
+            </div>
+        </main>
     )
 }
 
